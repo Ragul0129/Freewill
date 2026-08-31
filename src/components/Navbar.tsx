7809706ae0284729febba804c3aeb7c740625d4c
Ragul0129 <252414013.sclas@saveetha.com>
2026-08-31 10:10:51 +0530

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          FREEWILL
+        </Link>
+
+        <div className="navbar-links">
+          <Link to="/">Home</Link>
+          <Link to="/assessment">Assessment</Link>
+          <Link to="/booking">Counselling</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register" className="navbar-button">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
