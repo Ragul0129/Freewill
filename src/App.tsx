50079e0789cfe659f1a6665e647f5317eac190e4
Ragul0129 <252414013.sclas@saveetha.com>
2026-09-01 13:39:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Assessment from "./pages/Assessment";
-import AssessmentResult from "./pages/AssessmentResult";
-import Booking from "./pages/Booking";
-import ProtectedRoute from "./components/ProtectedRoute";
-import MyAppointment from "./pages/MyAppointment";
-import AdminLogin from "./pages/AdminLogin";
-import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
@@ -20,47 +9,7 @@ function App() {
       <Navbar />
 
       <Routes>
-
         <Route path="/" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="/assessment" element={<Assessment />} />
-
-        <Route path="/result" element={<AssessmentResult />} />
-
-        <Route path="/booking" element={<Booking />} />
-
-        <Route
-          path="/my-appointment"
-          element={<MyAppointment />}
-        />
-
-        <Route
-          path="/admin-login"
-          element={<AdminLogin />}
-        />
-
-        <Route
-          path="/admin-dashboard"
-          element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          }
-        />
-
       </Routes>
     </BrowserRouter>
   );
