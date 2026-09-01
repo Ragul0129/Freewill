b5988e16a3ede1f7802506bf172c2a16a7fb844d
Ragul0129 <252414013.sclas@saveetha.com>
2026-09-01 20:31:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,12 @@ import Register from "./pages/Register";
 function App() {
   return (
     <Routes>
-      {/* Home */}
       <Route path="/" element={<Home />} />
-
-      {/* Assessment */}
       <Route path="/assessment" element={<Assessment />} />
-
-      {/* Assessment Result */}
       <Route path="/result" element={<AssessmentResult />} />
-
-      {/* Counselling Booking */}
       <Route path="/booking" element={<Booking />} />
-
-      {/* Authentication */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-
-      {/* Fallback */}
       <Route path="*" element={<Home />} />
     </Routes>
   );
