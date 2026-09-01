3cd63a9f73e246377e01b85e95a500799e2ec7cd
Ragul0129 <252414013.sclas@saveetha.com>
2026-09-01 16:49:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import Assessment from "./pages/Assessment";
-import AssessmentResult from "./pages/AssessmentResult";
-import Booking from "./pages/Booking";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-
-        {/* Home */}
-        <Route path="/" element={<Home />} />
-
-        {/* Mental Wellness Assessment */}
-        <Route path="/assessment" element={<Assessment />} />
-
-        {/* Assessment Result */}
-        <Route path="/result" element={<AssessmentResult />} />
-
-        {/* Booking */}
-        <Route path="/booking" element={<Booking />} />
-
-        {/* Authentication */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* Fallback */}
-        <Route path="*" element={<Home />} />
-
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
